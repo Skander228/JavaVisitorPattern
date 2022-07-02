da47edf4e39573675c84cb1bd32ca492f63c5a11
Skander228 <kkhemiriskander@gmail.com>
2022-07-02 20:40:25 +0300

    Initial commit

---
+++ src/TreeElement.java
@@ -0,0 +1,16 @@
+/*public class TreeElement extends TreeNode {
+
+    private boolean _hasChild = false;
+    public TreeElement(int value, Color color, int depth) {
+        super(value, color, depth);
+    }
+    public boolean isLeaf() {
+        return !_hasChild;
+    }
+    @Override
+    public void addChild(Tree child) {
+        _hasChild = true;
+        super.addChild(child);
+    }
+
+}*/
